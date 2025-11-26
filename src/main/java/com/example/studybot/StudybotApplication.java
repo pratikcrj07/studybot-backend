a92b7d8d8df1265a0a367a2cb5011aac70e79fec
Pratik Humagain <pratikhumagaincr7@gmail.com>
2025-11-26 20:37:15 +0545

    Removed dotenv + fixed production env loading

--- src/main/java/com/example/studybot/StudybotApplication.java
+++ src/main/java/com/example/studybot/StudybotApplication.java
@@ -1,6 +1,5 @@
 package com.example.studybot;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +7,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudybotApplication {
 
     public static void main(String[] args) {
-        // Load bot.env file
-        Dotenv dotenv = Dotenv.configure()
-                .filename("bot.env")
-                .load();
-
-        // Set all env variables as system properties for Spring Boot
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
         SpringApplication.run(StudybotApplication.class, args);
     }
 }
+
+
+//package com.example.studybot;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class StudybotApplication {
+//
+//    public static void main(String[] args) {
+//        // Load bot.env file
+//        Dotenv dotenv = Dotenv.configure()
+//                .filename("bot.env")
+//                .load();
+//
+//        // Set all env variables as system properties for Spring Boot
+//        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+//
+//        SpringApplication.run(StudybotApplication.class, args);
+//    }
+//}
+
+
